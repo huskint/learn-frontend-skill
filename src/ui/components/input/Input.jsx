df925a6e356c143deccb79585c983ce525a26e45
bytrustu <bytrustu@gmail.com>
2022-12-18 21:06:04 +0900

    feat: 프로젝트 초기 설정

---
+++ src/ui/components/input/Input.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// TODO: Input Component를 작성하라.
+const Input = () => (
+  <div>Input</div>
+);
+export default Input;
---
+++ src/ui/hooks/useInput.jsx
@@ -0,0 +1,9 @@
+const useInput = () => {
+  // TODO: useInput Hook을 작성하라.
+  console.log('');
+  return {
+    value: '',
+  };
+};
+
+export default useInput;
