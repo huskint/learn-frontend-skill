1c0525be34585165d13804744d3981ad3133be8b
ShinjungOh <tlswjd9260@gmail.com>
2022-12-19 22:59:09 +0900

    [Mission1] feat: TextField UI 구현

--- src/ui/components/container/InputContainer.tsx
+++ src/ui/components/container/InputContainer.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
 
+import TextField from '../common/TextField';
+
 const InputContainer = () => {
   console.log('input Container');
   return (
-    <div style={{ padding: 100 }}>
-      InputContainer
-    </div>
+    <>
+      <TextField
+        label="label1"
+        id="label1"
+        placeholder="placeholder"
+        onChange={() => {
+        }}
+        value=""
+      />
+      <TextField
+        label="label2"
+        id="label2"
+        name="label2"
+        placeholder="placeholder"
+        readOnly
+        onChange={() => {
+        }}
+        value=""
+      />
+      <TextField
+        label="label3"
+        id="label3"
+        name="label3"
+        placeholder="placeholder"
+        disabled
+        onChange={() => {
+        }}
+        value=""
+      />
+      <TextField
+        label="label4"
+        id="label4"
+        error
+        errorMessage="오류가 발생했습니다."
+      />
+    </>
   );
 };
 
--- src/ui/components/input/Input.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-// TODO: Input Component를 작성하라.
-const Input = () => (
-  <div>Input</div>
-);
-export default Input;
